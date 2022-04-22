b84a2c935a5490a54c8a2481a051bd40f08743bb
YJH <jinmon0307@gmail.com>
2022-04-22 16:14:10 +0900

    메뉴 구조 2depth 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,7 +31,8 @@
       <v-divider />
 
       <v-list dense nav>
-        <v-list-item
+        <!-- 1 depth original menu s -->
+        <!-- <v-list-item
           v-for="item in menuItems"
           :key="item.title"
           link
@@ -45,7 +46,37 @@
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
-        </v-list-item>
+        </v-list-item> -->
+        <!-- 1 depth original menu s -->
+
+        <!-- 2depth menu s -->
+        <v-list-group
+          v-for="item in menuItems"
+          :key="item.title"
+          v-model="item.active"
+          no-action
+        >
+          <!-- :prepend-icon="item.action" -->
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="child in item.items"
+            :key="child.title"
+            link
+            :to="child.to"
+            @click="menuClick"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="child.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <!-- 2depth menu e -->
+
       </v-list>
     </v-navigation-drawer>
     <!-- 메뉴 영역 E -->
@@ -73,31 +104,58 @@ export default {
     // HelloWorld,
   },
 
-  data: () => ({
-    menuItems: [
-      { title: "Main", icon: "mdi-view-dashboard", to: "/" },
-      // { title: 'MenuTest1', icon: 'mdi-image', to: '/menu1'},
-      // { title: 'MenuTest2', icon: 'mdi-help-box', to: '/menu2'},
-      { title: "PaperList", icon: "mdi-help-box", to: "/paperList" },
-      { title: "PaperCreate", icon: "mdi-help-box", to: "/paperCreate" },
-      { title: "PaperCreate(wide)", icon: "mdi-help-box", to: "/paperCreate2" },
-      { title: "PaperDetail", icon: "mdi-help-box", to: "/paperDetail" },
-      { title: "PaymentList", icon: "mdi-help-box", to: "/paymentList" },
-      { title: "MemberList", icon: "mdi-help-box", to: "/memberList" },
-      { title: "MemberCreate", icon: "mdi-help-box", to: "/memberCreate" },
-      { title: "MemberCreate(narrow)", icon: "mdi-help-box", to: "/memberCreate2" },
-      { title: "BudgetList", icon: "mdi-help-box", to: "/budgetList" },
-    ],
-    isDrawMenu: true,
-    right: null,
-    pageTitle: ""
-  }),
-  created: function() {
+  data() {
+    return {
+      menuItems: [
+        // { title: 'Main', to: '/'},
+        {
+          title: 'Paper',
+          items: [
+            { title: 'PaperList', to: '/paperList' },
+            { title: 'PaperCreate', to: '/paperCreate' },
+            { title: 'PaperCreate2', to: '/paperCreate2' },
+            { title: 'PaperDetail', to: '/paperDetail' },
+          ],
+        },
+        {
+          title: 'Member',
+          items: [
+            { title: 'MemberList', to: '/memberList' },
+            { title: 'MemberCreate', to: '/memberCreate' },
+            { title: 'MemberDetail', to: '/memberDetail' },
+          ]
+        },
+        {
+          title: 'Budget',
+          items: [
+            { title: 'BudgetList', to: '/budgetList' },
+            { title: 'BudgetCreate', to: '/budgetCreate' },
+            { title: 'BudgetDetail', to: '/budgetDetail' },
+          ]
+        },
+
+        // { title: "Main", icon: "mdi-view-dashboard", to: "/" },
+        // { title: "PaperList", icon: "mdi-help-box", to: "/paperList" },
+        // { title: "PaperCreate", icon: "mdi-help-box", to: "/paperCreate" },
+        // { title: "PaperCreate(wide)", icon: "mdi-help-box", to: "/paperCreate2" },
+        // { title: "PaperDetail", icon: "mdi-help-box", to: "/paperDetail" },
+        // { title: "PaymentList", icon: "mdi-help-box", to: "/paymentList" },
+        // { title: "MemberList", icon: "mdi-help-box", to: "/memberList" },
+        // { title: "MemberCreate", icon: "mdi-help-box", to: "/memberCreate" },
+        // { title: "MemberCreate(narrow)", icon: "mdi-help-box", to: "/memberCreate2" },
+        // { title: "BudgetList", icon: "mdi-help-box", to: "/budgetList" },
+      ],
+      isDrawMenu: true,
+      right: null,
+      pageTitle: ""
+    }
+  },
+  created() {
     this.pageTitle = this.$route.name;
   },
-  mounted: function() {},
+  mounted() {},
   methods: {
-    menuClick: function() {
+    menuClick() {
       this.pageTitle = this.$route.name;
     }
   }
